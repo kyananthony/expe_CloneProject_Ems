72c6b99eb95e9f37b4175e645f9e36b8b5df3173
kyananthony <ktgonzaga@student.hau.edu.ph>
2024-10-01 22:40:51 +0800

    use for experimental purposes

---
+++ clone_Project_Ems/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for clone_Project_Ems project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clone_Project_Ems.settings')
+
+application = get_asgi_application()
